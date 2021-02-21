912f2557c95ce9cc58c1ef7047079bcc53a88721
Sheepishtowns <79208778+Sheepishtowns@users.noreply.github.com>
2021-02-21 14:30:21 +0800

    Update main.py

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    if not session.get("logged_in") and not session.get("admin"):
+    if not session.get("logged_in"):
         return redirect("/login")
     else:
         return redirect("/filesystem")

a415978a4660a065420c42ebb6e417863e03257d
Sheepishtowns <79208778+Sheepishtowns@users.noreply.github.com>
2021-02-21 14:25:49 +0800

    Update config.py

--- config.py
+++ config.py
@@ -5,18 +5,22 @@ port = 14389
 
 #all directory and files to be accessed remotely
 #if you add files please notice file extension is needed (eg. .pdf .jpg)
+#Add anything
 
 accessible_directories = {"public":#public category
-                              ["D:\yts\Pictures\Video Projects",
-                               "D:\of mice and men.pdf"]
+                              [#Add in here --> "/path/to/anything","/path/to/anything"
+                                
+                              ]  
                         , "protected":#protected category
-                              {"readonly":["D:\星战"],
-                               "useraccess":[["D:\Heaton","sheep"],            #first parameter is path      
-                                             ["D:\Heaton Application","sheep"]]#user access should specify user for each(second parameter)
-                               }
-                          } #public can be accessed by anyone, protected means read only or accessed with specific username
+                               "useraccess":[
+                                  #add in here --> ["path/to/anything", "user"],["path/to/anything", "user2"]
+                               ]                                               #first parameter is path
+                                                                               #user access should specify user for each(second parameter)
+                                                                               #user config down below
+                          } #public can be accessed by anyone, protected can be accessed with specific username
 
 
 
 #users specify username and password
+#for access protected ones
 users = {"sheep":"123"}

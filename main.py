2794d414c479226437eac5a533d34f62334d32bb
Sheepishtowns <79208778+Sheepishtowns@users.noreply.github.com>
2021-02-21 14:29:48 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,21 +7,12 @@ from pathlib import Path
 
 # NOTE: This program should be run as root or administrator to prevent file privilege problems
 
-# ---validates user input----------------------------------------------
+# ---validates config input----------------------------------------------
 for item in accessible_directories["public"]:
     if not os.path.exists(item):
         print("\n !path or file '" + item + "' does not exist")
         accessible_directories["public"].remove(item)
 
-for item in accessible_directories["private"]:
-    if not os.path.exists(item):
-        print("\n !path or file '" + item + "' does not exist")
-        accessible_directories["private"].remove(item)
-
-for item in accessible_directories["protected"]["readonly"]:
-    if not os.path.exists(item):
-        print("\n !path or file '" + item + "' does not exist")
-        accessible_directories["private"].remove(item)
 
 for item in accessible_directories["protected"]["useraccess"]:
     if not os.path.exists(item[0]):
@@ -31,10 +22,6 @@ for item in accessible_directories["protected"]["useraccess"]:
         print("\n !protected file or path '" + item[0] + "'. assigned user '" + item[1] + "' does not exist")
         accessible_directories["protected"]["useraccess"].remove(item)
 
-for item in accessible_directories["protected"]["readonly"]:
-    if not os.path.exists(item):
-        print("\n !path or file '" + item + "' does not exist")
-        accessible_directories["protected"]["readonly"].remove(item)
 
 # -----------------require user login-----------------------
 app = Flask(__name__)
@@ -44,10 +31,8 @@ app = Flask(__name__)
 def home():
     if not session.get("logged_in") and not session.get("admin"):
         return redirect("/login")
-    elif session.get("admin"):
-        return redirect("/admin")
     else:
-        return render_template("filesystem.html")
+        return redirect("/filesystem")
 
 
 @app.route("/filesystem")
@@ -87,7 +72,7 @@ def get_directory():
         elif os.path.isdir(request.form["dir"]):
             for item in all_stuff:
                 if Path(item) in Path(request.form["dir"]).parents or request.form["dir"] == item:
-                    print("hey")
+                    
                     try:
                         subs = os.walk(request.form["dir"])
                         for root, directories, files in subs:
@@ -111,12 +96,6 @@ def login():
             print("user \"" + request.form['username'] + "\" logged in. IP: " + request.remote_addr)
         else:
             flash('Login failed!')
-    elif request.form['username'] == "":
-        if request.form['password'] == key:
-            session['admin'] = True
-            print("the admin logged in. IP: " + request.remote_addr)
-        else:
-            flash('Login failed!')
     else:
         flash('Login failed!')
     return redirect("/")
@@ -133,44 +112,6 @@ def logout():
     session["user"] = ""
     session["admin"] = False
     return home()
-
-
-# admin page
-@app.route("/admin")
-def admin():
-    if session.get("admin"):
-        # if
-        html_string = ""
-        for item in accessible_directories["public"]:
-            html_string = html_string + "<li>" + item + "</li>"
-        flash(Markup(html_string), "public_list")
-        return render_template("admin.html")
-    else:
-        return "Go away!"
-
-
-@app.route("/admin", methods=["POST"])
-def config():
-
-    type = request.form.get('form-id')
-    if type == "public":
-        new_path = request.form.get('path')
-        if new_path == "":
-            flash("Empty", "notice")
-        elif not os.path.isdir(new_path) and not os.path.isfile(new_path):
-            flash("Path not exist", "notice")
-        elif new_path in accessible_directories["public"]:
-            flash("Path already added", "notice")
-        else:
-            accessible_directories["public"].append(new_path)
-
-    html_string = ""
-    for item in accessible_directories["public"]:
-        html_string = html_string + "<li>" + item + "</li>"
-    flash(Markup(html_string), "public_list")
-
-    return render_template("admin.html")
-
 
 # run application
 if __name__ == "__main__":
